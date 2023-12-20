d762a3accbb9e13ea0638a01689f64602418ad0c
Fabrice Sznajderman <fabszn@protonmail.com>
2023-12-20 21:58:00 +0100

    wip bootstrap

---
+++ project/Dependencies.scala
@@ -0,0 +1,18 @@
+import Dependencies.version._
+import sbt._
+
+
+object Dependencies {
+
+    object version{
+        lazy val zioVersion                 = "2.0.2"
+    }
+
+
+    lazy val zio = Seq(
+        "dev.zio" %% "zio"          % zioVersion,
+        "dev.zio" %% "zio-test"     % zioVersion % "test,it",
+        "dev.zio" %% "zio-test-sbt" % zioVersion % "test,it"
+    )
+
+}
