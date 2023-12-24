271aebef838005302a4d8a739b99b06845c67bfe
Fabrice Sznajderman <fabszn@protonmail.com>
2023-12-24 08:54:59 +0100

    front OK

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -1,11 +1,24 @@
 <template>
-
-<p>Hello World</p>
-<button class="btn btn-primary">Login</button>
-
+  <div class="form-floating">
+    <input
+      type="email"
+      class="form-control"
+      id="floatingInput"
+      placeholder="name@example.com"
+    />
+    <label for="floatingInput">Email address</label>
+  </div>
+  <div class="form-floating">
+    <input
+      type="password"
+      class="form-control"
+      id="floatingPassword"
+      placeholder="Password"
+    />
+    <label for="floatingPassword">Password</label>
+  </div>
+  <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
 </template>
 
 <style scoped>
-
-
 </style>
